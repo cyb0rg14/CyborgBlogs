0b19458ea01b81e718cbbf0af53c506c49a1c738
cyb0rg14 <techtitanbot@gmail.com>
2024-02-11 17:41:42 +0530

    starter pack uploaded

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+    title: 'Cyborg Blogs',
+    author: 'cyb0rg14',
+    headerTitle: 'Cyborg Blogs',
+    description: 'Everything related to tech at one place.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'cyborgdomain@proton.me', 
+    github: 'https://github.com/cyb0rg14',
+    twitter: 'https://twitter.com/cyb0rg14',
+    instagram: 'https://instagram.com/cyborgdomain',
+    youtube: 'https://youtube.com/',
+    linkedin: 'https://www.linkedin.com/in/cyb0rg14/',
+    dribbble: 'https://www.dribbble.com',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
