896779123762b7e4809345fb73d58af7d0e2c87f
cyb0rg14 <techtitanbot@gmail.com>
2024-02-12 23:21:31 +0530

    updated the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -17,7 +17,7 @@ const Footer = () => {
   return (
     <footer className="mt-16 rounded-2xl bg-dark dark:bg-accentDark/90 m-2 sm:m-10 flex flex-col items-center text-light dark:text-dark">
       <h3 className="mt-16 font-medium dark:font-bold text-center capitalize text-2xl sm:text-3xl lg:text-4xl px-4">
-        Coding Tips | Updates | Guides
+        Coding Tips | Career Guidance | Tech Updates 
       </h3>
       <p className="mt-5 px-4 text-center w-full sm:w-3/5 font-light dark:font-medium text-sm sm:text-base">
         Subscribe to learn about new technology and updates. Join over 1000+
